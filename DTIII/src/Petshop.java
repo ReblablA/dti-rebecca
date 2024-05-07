432ac7a40e7c80ca428225659542c6e67fa32d28
ReblablA <111395018+ReblablA@users.noreply.github.com>
2024-05-07 19:21:52 -0300

    Update Petshop.java

--- DTIII/src/Petshop.java
+++ DTIII/src/Petshop.java
@@ -11,7 +11,7 @@ public class Petshop {
     private final double precoPequenoFimDeSemana;
     private final double precoGrandeFimDeSemana;
 
-    // Construtor
+   
     public Petshop(String nome, double distancia, double precoPequenoDiaUtil, double precoGrandeDiaUtil, double precoPequenoFimDeSemana, double precoGrandeFimDeSemana) {
         this.nome = nome;
         this.distancia = distancia;
@@ -21,7 +21,7 @@ public class Petshop {
         this.precoGrandeFimDeSemana = precoGrandeFimDeSemana;
     }
 
-    // Método para calcular o preço total dos banhos para uma determinada quantidade de cães pequenos e grandes em uma data específica
+   
     public double calcularPrecoTotal(String data, int qtdPequenos, int qtdGrandes) {
         double precoTotal;
         if (isFimDeSemana(data)) {
@@ -32,16 +32,16 @@ public class Petshop {
         return precoTotal;
     }
 
-    // Método auxiliar para verificar se a data é fim de semana
+ 
     private boolean isFimDeSemana(String data) {
-        // Convertendo a string de data para LocalDate
+       
         LocalDate date = LocalDate.parse(data, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 
-        // Verificando se o dia da semana é sábado ou domingo
+    
         return date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY;
     }
 
-    // Getters
+
     public String getNome() {
         return nome;
     }
